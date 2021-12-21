83ab1ef5dc3233ed0e2bb31853af4d12c564ccda
Tory Clasen <ToryClasen@Gmail.com>
2021-12-20 22:27:44 -0500

    fix: split out extra trailing space test from invalid test
    Signed-off-by: Tory Clasen <ToryClasen@Gmail.com>

--- gitbom_test.go
+++ gitbom_test.go
@@ -104,8 +104,9 @@ func TestInvalidOpaqueGitBom(t *testing.T) {
 
 	_, err = NewOpaqueGitBom("23294b0610492cf55c1c4835216f20d376a287dg")
 	assert.Error(t, err)
-
-	_, err = NewOpaqueGitBom("23294b0610492cf55c1c4835216f20d376a287dd ")
+}
+func TestInvalidOpaqueGitBom_ExtraTrailingSpace(t *testing.T) {
+	_, err := NewOpaqueGitBom("23294b0610492cf55c1c4835216f20d376a287dd ")
 	assert.Error(t, err)
 }
 
